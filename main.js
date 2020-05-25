b56a84f0f04e8c98aa6c8e3ccdb0b45a486f3922
Humberto Lagomarsino G <h.lagomarsino.g@gmail.com>
2020-05-25 16:42:15 -0400

    upload Archivos

--- main.js
+++ main.js
@@ -1,43 +1,31 @@
 window.onload = function () {
 
-    let tipoCarta = ["pica", "corazon", "trebol", "diamante"];
+    let tipoCarta = ["&spades;", "&hearts;", "&clubs;", "&diams;"];
     let aleatorioNumero = Math.floor(Math.random() * 13 + 1);
     let pinta = Math.floor(Math.random() * tipoCarta.length);
+    console.log(tipoCarta[pinta]);
 
-    switch (pinta) {
-        case 0: pinta = "&spades;";
+    switch (aleatorioNumero) {
+        case 11: aleatorioNumero = "J";
             break;
-        case 1: pinta = "&hearts;";
+        case 12: aleatorioNumero = "Q";
             break;
-        case 2: pinta = "&clubs;";
+        case 13: aleatorioNumero = "K";
             break;
-        case 3: pinta = "&diams;";
-            break;
-    };
-
-
-    if (aleatorioNumero == 11) {
-        aleatorioNumero = "J";
-    } else if (aleatorioNumero == 12) {
-        aleatorioNumero = "Q";
-    } else if (aleatorioNumero == 13) {
-        aleatorioNumero = "K";
     }
-
 
     let h1Carta = document.querySelector("#numero");
     let divCarta = document.querySelector("#tipoCarta");
     let divCarta2 = document.querySelector("#tipoCarta2");
 
-    if(pinta == "&hearts;" || pinta == "&diams;"){
-        h1Carta.style.color="red";
+    if (tipoCarta[pinta] == "&hearts;" || tipoCarta[pinta] == "&diams;") {
+        h1Carta.style.color = "red";
         divCarta.classList.add("estiloRojo");
         divCarta2.classList.add("estiloRojo");
     }
 
-
-    divCarta.innerHTML = pinta;
-    divCarta2.innerHTML = pinta
+    divCarta.innerHTML = tipoCarta[pinta];
+    divCarta2.innerHTML = tipoCarta[pinta]
     h1Carta.innerHTML = aleatorioNumero;
 
 };
